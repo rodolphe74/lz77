2ed87ca941f9a0a8383968f82281a9274fe81a07
rodolphe74 <rcordenie@gmail.com>
2023-09-01 05:44:11 +0200

    image compressed header samples

--- tolz.c
+++ tolz.c
@@ -1,9 +1,8 @@
 #include "lz77.h"
-#include "beast00077.h"
+// #include "beast00077.h"
 // #include "nimoy0077.h"
 // #include "fouAPi0077.h"
-// #include "arton50077.h"
-// #include "thomso0077.h"
+#include "arton50077.h"
 
 // #define INPUT_SIZE 1024
 // #define OUTPUT_SIZE 1024
@@ -50,20 +49,13 @@ asm {
 int main(/*int argc, char *argv[]*/)
 {
     _160();
-    _PALETTE(beast000.pal);
+    _PALETTE(arton500.pal);
 
     int *screenStart = (int *)0x4000;
     UCHAR *screenStartChar = (UCHAR *)screenStart;
 
 
     // Trying to understand thomson 160 frame buffer :
-    // UCHAR *ptr = (UCHAR *) screenStart;
-    // UCHAR *ptr2 = (UCHAR *) (screenStart + 4096);
-    // uncompress(nimoy00.screen, nimoy00.len, (UCHAR *) screenStart, 16384);
-    // for (int i = 0; i < 4000; i++) {
-    //     ptr[i] = ((UCHAR) i) % 255;
-    //     ptr[i + 4000] = ((UCHAR) i) % 255;
-    // }
 
     // // Plan A
     // for (int i = 0; i < 4000; i++) {
@@ -94,9 +86,9 @@ int main(/*int argc, char *argv[]*/)
 
     // Understood:
     initParameters(511, 9, 15, 4);
-    uncompress(beast000.mapA, beast000.lenA, (UCHAR *) screenStartChar, 8000);
+    uncompress(arton500.mapA, arton500.lenA, (UCHAR *) screenStartChar, 8000);
     _SWITCH_B();
-    uncompress(beast000.mapB, beast000.lenB, (UCHAR *) screenStartChar, 8000);
+    uncompress(arton500.mapB, arton500.lenB, (UCHAR *) screenStartChar, 8000);
 
     return 0;
 }
